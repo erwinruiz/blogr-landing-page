3c63b05e4703c6b234f7f78f7ced854baebf82ad
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-09-17 19:29:02 -0700

    Add content component (add first part)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Content from "./components/Content";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
       <Home />
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,35 @@
+import classes from "./Content.module.css";
+
+function Content() {
+  return (
+    <section className={classes.container}>
+      <h2 className={classes.firstTitle}>Designed for the future</h2>
+      <img
+        className={classes.img}
+        src="./images/illustration-editor-mobile.svg"
+        alt="illustration-editor-mobile"
+      />
+      <div className={classes.textContent}>
+        <h2>Introducing an extensible editor</h2>
+        <p>
+          Blogr features an exceedingly intuitive interface which lets you focus
+          on one thing: creating content. The editor supports management of
+          multiple blogs and allows easy manipulation of embeds such as images,
+          videos, and Markdown. Extensibility with plugins and themes provide
+          easy ways to add functionality or change the looks of a blog.
+        </p>
+      </div>
+      <div className={classes.textContent}>
+        <h2>Robust content management</h2>
+        <p>
+          Flexible content management enables users to easily move through
+          posts. Increase the usability of your blog by adding customized
+          categories, sections, format, or flow. With this functionality, you’re
+          in full control.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Content;
