4e38808d604f815713419772e5b476492d530548
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-09-22 17:45:02 -0700

    Adapt first section component to desktop design

--- src/components/Content/FirstSection.js
+++ src/components/Content/FirstSection.js
@@ -5,11 +5,11 @@ function FirstSection() {
     <section className={classes.container}>
       <h2 className={classes.firstTitle}>Designed for the future</h2>
       <img
-        className={classes.img}
+        className={classes.mobileImg}
         src="./images/illustration-editor-mobile.svg"
         alt="illustration-editor-mobile"
       />
-      <div className={classes.textContent}>
+      <div className={`${classes.textContent} ${classes.firstTextContent}`}>
         <h2>Introducing an extensible editor</h2>
         <p>
           Blogr features an exceedingly intuitive interface which lets you focus
@@ -19,7 +19,7 @@ function FirstSection() {
           easy ways to add functionality or change the looks of a blog.
         </p>
       </div>
-      <div className={classes.textContent}>
+      <div className={`${classes.textContent} ${classes.secondTextContent}`}>
         <h2>Robust content management</h2>
         <p>
           Flexible content management enables users to easily move through
